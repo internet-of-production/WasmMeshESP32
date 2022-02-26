6b6ab8a36946d1f13d2c07aeef9869fd209e0790
Otoya Nakakaze <otoya.nakakaze@rwth-aachen.de>
2022-02-26 19:05:14 +0100

    A simple example of the BLE-client is implemented

--- BLE-communication/client/src/main.cpp
+++ BLE-communication/client/src/main.cpp
@@ -25,26 +25,50 @@
 #define WASM_STACK_SLOTS    4000
 #define CALC_INPUT  2
 #define FATAL(func, msg) { Serial.print("Fatal: " func " "); Serial.println(msg); return; }
-#define CHANNEL 0
 #define MAX_PAYLOAD_SIZE 240 //ESP_NOW_MAX_DATA_LEN (250byte) is not usable due to packet's structure. (it also sends packets flag, total packet size, offset, etc.) 
 #define bleServerName "Wasm_ESP32"
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-#define SERVICE_UUID "ed6a9e2f-2408-4b78-a3d6-3aa55f71a38a"
+//Default Temperature is in Celsius
+//Comment the next line for Temperature in Fahrenheit
+#define temperatureCelsius
 
-// Temperature Characteristic and Descriptor
+// BLE Service. Set your service's UUID
+static BLEUUID bmeServiceUUID("ed6a9e2f-2408-4b78-a3d6-3aa55f71a38a");
+
+// BLE Characteristics
 #ifdef temperatureCelsius
-  BLECharacteristic bmeTemperatureCelsiusCharacteristics("cba1d466-344c-4be3-ab3f-189f80dd7518", BLECharacteristic::PROPERTY_NOTIFY);
-  BLEDescriptor bmeTemperatureCelsiusDescriptor(BLEUUID((uint16_t)0x2902));
+  //Temperature Celsius Characteristic
+  static BLEUUID temperatureCharacteristicUUID("cba1d466-344c-4be3-ab3f-189f80dd7518");
 #else
-  BLECharacteristic bmeTemperatureFahrenheitCharacteristics("f78ebbff-c8b7-4107-93de-889a6a06d408", BLECharacteristic::PROPERTY_NOTIFY);
-  BLEDescriptor bmeTemperatureFahrenheitDescriptor(BLEUUID((uint16_t)0x2901));
+  //Temperature Fahrenheit Characteristic
+  static BLEUUID temperatureCharacteristicUUID("f78ebbff-c8b7-4107-93de-889a6a06d408");
 #endif
 
-// Humidity Characteristic and Descriptor
-BLECharacteristic bmeHumidityCharacteristics("ca73b3ba-39f6-4ab3-91ae-186dc9577d99", BLECharacteristic::PROPERTY_NOTIFY);
-BLEDescriptor bmeHumidityDescriptor(BLEUUID((uint16_t)0x2903));
+// Humidity Characteristic
+static BLEUUID humidityCharacteristicUUID("ca73b3ba-39f6-4ab3-91ae-186dc9577d99");
+
+//Flags stating if should begin connecting and if the connection is up
+static boolean doConnect = false;
+static boolean connected = false;
+
+//Address of the peripheral device. Address will be found during scanning...
+static BLEAddress *pServerAddress;
+ 
+//Characteristicd that we want to read
+static BLERemoteCharacteristic* temperatureCharacteristic;
+static BLERemoteCharacteristic* humidityCharacteristic;
+
+//Activate notify
+const uint8_t notificationOn[] = {0x1, 0x0};
+const uint8_t notificationOff[] = {0x0, 0x0};
+
+//Variables to store temperature and humidity
+char* temperatureChar;
+char* humidityChar;
+
+//Flags to check whether new temperature and humidity readings are available
+boolean newTemperature = false;
+boolean newHumidity = false;
 
 IM3Environment env;
 IM3Runtime runtime;
@@ -71,47 +95,70 @@ int currentTransmitOffset = 0;
 int numberOfPackets = 0;
 byte sendNextPacketFlag = 0;
 
-//! If a client is connected to the server, the state is true. If the client disconnects, the boolean variable changes to false.
-bool deviceConnected = false;
 
-float temp;
-float tempF;
-float hum;
 
-// Timer variables
-unsigned long lastTime = 0;
-unsigned long timerDelay = 30000;
-
-/**
- * @fn 
- * Get WiFi channel
- * @param ssid SSID of the router (const char*)
- */
-int32_t getWiFiChannel(const char *ssid) {
-  if (int32_t n = WiFi.scanNetworks()) {
-      for (uint8_t i=0; i<n; i++) {
-          if (!strcmp(ssid, WiFi.SSID(i).c_str())) {
-              return WiFi.channel(i);
-          }
-      }
-  }
-  return 0;
+//When the BLE Server sends a new temperature reading with the notify property
+static void temperatureNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, 
+                                        uint8_t* pData, size_t length, bool isNotify) {
+  //store temperature value
+  temperatureChar = (char*)pData;
+  newTemperature = true;
 }
 
+//When the BLE Server sends a new humidity reading with the notify property
+static void humidityNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic, 
+                                    uint8_t* pData, size_t length, bool isNotify) {
+  //store humidity value
+  humidityChar = (char*)pData;
+  newHumidity = true;
+}
 
-/**
- * @class MyServerCallbacks
- * @brief Setup callbacks onConnect and onDisconnect. (BLEServerCallbacks)
- */
+//Connect to the BLE Server that has the name, Service, and Characteristics
+bool connectToServer(BLEAddress pAddress) {
+   BLEClient* pClient = BLEDevice::createClient();
+ 
+  // Connect to the remove BLE Server.
+  pClient->connect(pAddress);
+  Serial.println("Connected to server");
+ 
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(bmeServiceUUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(bmeServiceUUID.toString().c_str());
+    return (false);
+  }
+ 
+  // Obtain a reference to the characteristics in the service of the remote BLE server.
+  temperatureCharacteristic = pRemoteService->getCharacteristic(temperatureCharacteristicUUID);
+  humidityCharacteristic = pRemoteService->getCharacteristic(humidityCharacteristicUUID);
 
-class MyServerCallbacks: public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
-    deviceConnected = true;
-  };
-  void onDisconnect(BLEServer* pServer) {
-    deviceConnected = false;
+  if (temperatureCharacteristic == nullptr || humidityCharacteristic == nullptr) {
+    Serial.print("Failed to find our characteristic UUID");
+    return false;
+  }
+  Serial.println("Found our characteristics");
+ 
+  //Assign callback functions for the Characteristics
+  temperatureCharacteristic->registerForNotify(temperatureNotifyCallback);
+  humidityCharacteristic->registerForNotify(humidityNotifyCallback);
+  return true;
+}
+
+//Callback function that gets called, when another device's advertisement has been received
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    if (advertisedDevice.getName() == bleServerName) { //Check if the name of the advertiser matches
+      advertisedDevice.getScan()->stop(); //Scan can be stopped, we found what we are looking for
+      pServerAddress = new BLEAddress(advertisedDevice.getAddress()); //Address of advertiser is the one we need
+      doConnect = true; //Set indicator, stating that we are ready to connect
+      Serial.println("Device found. Connecting!");
+    }
   }
 };
+
+
+
 
 
 /**
@@ -322,38 +369,18 @@ void setup(){
   //set up for wasm
   run_wasm(NULL);
 
-  // Create the BLE Device
-  BLEDevice::init(bleServerName);
+  //Init BLE device
+  BLEDevice::init("");
 
   Serial.println("ble initilized");
 
-  // Create the BLE Server
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-
-  // Create the BLE Service
-  BLEService *bmeService = pServer->createService(SERVICE_UUID);
-
-  // Create BLE Characteristics and Create a BLE Descriptor
-  // Temperature
-  #ifdef temperatureCelsius
-    bmeService->addCharacteristic(&bmeTemperatureCelsiusCharacteristics);
-    bmeTemperatureCelsiusDescriptor.setValue("BME temperature Celsius");
-    bmeTemperatureCelsiusCharacteristics.addDescriptor(&bmeTemperatureCelsiusDescriptor);
-  #else
-    bmeService->addCharacteristic(&bmeTemperatureFahrenheitCharacteristics);
-    bmeTemperatureFahrenheitDescriptor.setValue("BME temperature Fahrenheit");
-    bmeTemperatureFahrenheitCharacteristics.addDescriptor(&bmeTemperatureFahrenheitDescriptor);
-  #endif 
-
-  // Start the service
-  bmeService->start();
-
-  // Start advertising
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pServer->getAdvertising()->start();
-  Serial.println("Waiting a client connection to notify...");
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 30 seconds.
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(30);
 
 }
  
@@ -380,47 +407,28 @@ void loop(){
   if (sendNextPacketFlag)
     sendNextPacket();*/
 
-  if (deviceConnected) {
-    if ((millis() - lastTime) > timerDelay) {
-      // Read temperature as Celsius (the default)
-      temp = 20.0;
-      // Fahrenheit
-      tempF = temp*1.8 +32;
-      // Read humidity
-      hum = 60.0;
-      //Notify temperature. Convert double to ASCII Using dtostrf()
-      #ifdef temperatureCelsius
-        static char temperatureCTemp[6];
-        dtostrf(temp, 6, 2, temperatureCTemp);
-        //Set temperature Characteristic value and notify connected client
-        bmeTemperatureCelsiusCharacteristics.setValue(temperatureCTemp);
-        bmeTemperatureCelsiusCharacteristics.notify();
-        Serial.print("Temperature Celsius: ");
-        Serial.print(temp);
-        Serial.print(" ºC");
-      #else
-        static char temperatureFTemp[6];
-        dtostrf(tempF, 6, 2, temperatureFTemp);
-        //Set temperature Characteristic value and notify connected client
-        bmeTemperatureFahrenheitCharacteristics.setValue(temperatureFTemp);
-        bmeTemperatureFahrenheitCharacteristics.notify();
-        Serial.print("Temperature Fahrenheit: ");
-        Serial.print(tempF);
-        Serial.print(" ºF");
-      #endif
-      
-      //Notify humidity reading from BME
-      static char humidityTemp[6];
-      dtostrf(hum, 6, 2, humidityTemp);
-      //Set humidity Characteristic value and notify connected client
-      bmeHumidityCharacteristics.setValue(humidityTemp);
-      bmeHumidityCharacteristics.notify();   
-      Serial.print(" - Humidity: ");
-      Serial.print(hum);
-      Serial.println(" %");
-      
-      lastTime = millis();
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
+  // connected we set the connected flag to be true.
+  if (doConnect == true) {
+    if (connectToServer(*pServerAddress)) {
+      Serial.println("We are now connected to the BLE Server.");
+      //Activate the Notify property of each Characteristic
+      temperatureCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
+      humidityCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
+      connected = true;
+    } else {
+      Serial.println("We have failed to connect to the server; Restart your device to scan for nearby BLE server again.");
     }
+    doConnect = false;
+  }
+
+  //if new temperature readings are available, print vales and reset flags
+  if (newTemperature && newHumidity){
+    newTemperature = false;
+    newHumidity = false;
+    Serial.println(temperatureChar);
+    Serial.println(humidityChar);
   }
   
   wasm_task();
